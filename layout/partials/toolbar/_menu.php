546b0d18e91dd64be1ff73ba5cecb69ebe107b8b
RAIS <rais.ratamto@gmail.com>
2025-12-30 17:18:25 +0700

    Update toolbar menu: Dashboard + Rawat Jalan/Inap with active state (placeholders)

--- layout/partials/toolbar/_menu.php
+++ layout/partials/toolbar/_menu.php
@@ -6,49 +6,24 @@
 <div class="app-toolbar-menu menu menu-title-gray-800 menu-state-primary flex-wrap fs-5 fw-semibold w-100">
 	<!--begin::Menu item-->
 	<div class="menu-item">
-		<a class="menu-link py-4 active" href="?page=index">
-			<span class="menu-title">
-				Summary </span>
+		<a class="menu-link py-4 <?= $page === 'index' ? 'active' : '' ?>" href="?page=index">
+			<span class="menu-title">Dashboard</span>
 		</a>
 	</div>
 	<!--end::Menu item-->
+
 	<!--begin::Menu item-->
 	<div class="menu-item">
-		<a class="menu-link py-4 " href="?page=apps/projects/list">
-			<span class="menu-title">
-				Projects </span>
+		<a class="menu-link py-4 <?= $page === 'rawat-jalan' ? 'active' : '' ?>" href="?page=rawat-jalan">
+			<span class="menu-title">Rawat Jalan</span>
 		</a>
 	</div>
 	<!--end::Menu item-->
+
 	<!--begin::Menu item-->
 	<div class="menu-item">
-		<a class="menu-link py-4 " href="?page=apps/subscriptions/list">
-			<span class="menu-title">
-				Subscriptions </span>
-		</a>
-	</div>
-	<!--end::Menu item-->
-	<!--begin::Menu item-->
-	<div class="menu-item">
-		<a class="menu-link py-4 " href="?page=apps/file-manager/folders">
-			<span class="menu-title">
-				Files </span>
-		</a>
-	</div>
-	<!--end::Menu item-->
-	<!--begin::Menu item-->
-	<div class="menu-item">
-		<a class="menu-link py-4 " href="?page=apps/support-center/contact">
-			<span class="menu-title">
-				Support </span>
-		</a>
-	</div>
-	<!--end::Menu item-->
-	<!--begin::Menu item-->
-	<div class="menu-item">
-		<a class="menu-link py-4 " href="?page=apps/customers/list">
-			<span class="menu-title">
-				Customers </span>
+		<a class="menu-link py-4 <?= $page === 'rawat-inap' ? 'active' : '' ?>" href="?page=rawat-inap">
+			<span class="menu-title">Rawat Inap</span>
 		</a>
 	</div>
 	<!--end::Menu item-->
