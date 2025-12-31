dcfd3afda9238e2ef80a12307c472817cf632be1
Rais Ratamto <rais.ratamto@gmail.com>
2025-12-31 13:27:42 +0700

    Update
    Rapihin css, js, exclude page-title dan buat toolbar lebih rapat ke header

--- layout/partials/_toolbar.php
+++ layout/partials/_toolbar.php
@@ -6,12 +6,9 @@ $__emr_root = defined('EMR_ROOT') ? EMR_ROOT : realpath(__DIR__ . '/../../');
 $tabs = $GLOBALS['EMR_TABS'] ?? null;
 ?>
 <!--begin::Toolbar-->
-<div id="kt_app_toolbar" class="app-toolbar  py-7 pt-lg-15 pb-lg-5 ">
+<div id="kt_app_toolbar" class="app-toolbar py-7 pt-lg-5 pb-lg-5 ">
     <div id="kt_app_toolbar_container" class="app-container container-xxl d-flex align-items-stretch ">
         <div class="app-toolbar-container d-flex flex-column flex-row-fluid">
-
-            <?php include $__emr_root . '/layout/partials/toolbar/_page-title.php'; ?>
-
             <div class="d-flex justify-content-between flex-wrap gap-4 gap-lg-10">
                 <?php if (is_array($tabs)) : ?>
                     <?php include $__emr_root . '/apps/simrs/partials/_tabs.php'; ?>
@@ -19,7 +16,6 @@ $tabs = $GLOBALS['EMR_TABS'] ?? null;
                     <?php include $__emr_root . '/layout/partials/toolbar/_menu.php'; ?>
                 <?php endif; ?>
             </div>
-
         </div>
     </div>
 </div>
