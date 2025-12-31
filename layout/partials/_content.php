857a0ac8f8f6c772d156cd7b83c9b351c643c67a
Rais Ratamto <rais.ratamto@gmail.com>
2025-12-31 10:45:00 +0700

    Simplify content and user account menu structure
    Replaced the main content partial with placeholder content and significantly reduced the user account menu, removing project, subscription, statements, and language options. The theme mode selector is now included directly, and the sign out link is clarified. These changes streamline the UI and remove unused or unnecessary menu items.

--- layout/partials/_content.php
+++ layout/partials/_content.php
@@ -5,57 +5,7 @@ $__emr_root = defined('EMR_ROOT') ? EMR_ROOT : realpath(__DIR__ . '/../../');
 <!--begin::Content-->
 <div id="kt_app_content" class="app-content ">
 
-    <!--begin::Row-->
-    <div class="row gx-5 gx-xl-10">
-        <div class="col-xxl-4 mb-5 mb-xl-10">
-            <?php include $__emr_root . '/partials/widgets/charts/_widget-27.php'; ?>
-        </div>
-        <div class="col-xxl-4 mb-5 mb-xl-10">
-            <?php include $__emr_root . '/partials/widgets/charts/_widget-28.php'; ?>
-        </div>
-        <div class="col-xxl-4 mb-5 mb-xl-10">
-            <?php include $__emr_root . '/partials/widgets/lists/_widget-9.php'; ?>
-        </div>
-    </div>
-
-    <div class="row gx-5 gx-xl-10">
-        <div class="col-xl-6 mb-5 mb-xl-10">
-            <?php include $__emr_root . '/partials/widgets/tables/_widget-9.php'; ?>
-        </div>
-        <div class="col-xl-6 mb-5 mb-xl-10">
-            <?php include $__emr_root . '/partials/widgets/tables/_widget-10.php'; ?>
-        </div>
-    </div>
-
-    <div class="row gx-5 gx-xl-10">
-        <div class="col-xl-6 mb-5 mb-xl-10">
-            <?php include $__emr_root . '/partials/widgets/charts/_widget-15.php'; ?>
-        </div>
-        <div class="col-xl-6 mb-5 mb-xl-10">
-            <?php include $__emr_root . '/partials/widgets/tables/_widget-11.php'; ?>
-        </div>
-    </div>
-
-    <div class="row gx-5 gx-xl-10">
-        <div class="col-xl-4 mb-5 mb-xl-10">
-            <?php include $__emr_root . '/partials/widgets/charts/_widget-29.php'; ?>
-        </div>
-        <div class="col-xl-8 mb-5 mb-xl-10">
-            <?php include $__emr_root . '/partials/widgets/charts/_widget-24.php'; ?>
-        </div>
-    </div>
-
-    <div class="row gx-5 gx-xl-10">
-        <div class="col-xl-4 mb-5 mb-xl-0">
-            <?php include $__emr_root . '/partials/widgets/lists/_widget-12.php'; ?>
-        </div>
-        <div class="col-xl-4 mb-5 mb-xl-0">
-            <?php include $__emr_root . '/partials/widgets/charts/_widget-31.php'; ?>
-        </div>
-        <div class="col-xl-4 mb-5 mb-xl-0">
-            <?php include $__emr_root . '/partials/widgets/charts/_widget-30.php'; ?>
-        </div>
-    </div>
+placeholder content
 
 </div>
 <!--end::Content-->
