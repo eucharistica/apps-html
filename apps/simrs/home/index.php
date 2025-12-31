01af7155a11925abb9d1d00b91fdc31215925966
RAIS <rais.ratamto@gmail.com>
2025-12-31 09:36:35 +0700

    Restructure entrypoint: root login index + move app to apps/simrs/home; add auth sign-in handler (placeholder)

---
+++ apps/simrs/home/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--begin::Head-->
+
+<head>
+    <base href="" />
+    <title>Metronic - The World's #1 Selling Tailwind CSS & Bootstrap Admin Template by KeenThemes</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title"
+        content="Metronic - The World's #1 Selling Tailwind CSS & Bootstrap Admin Template by KeenThemes" />
+    <meta property="og:url" content="https://keenthemes.com/metronic" />
+    <meta property="og:site_name" content="Metronic by Keenthemes" />
+    <link rel="canonical" href="http://preview.keenthemes.com?page=index" />
+    <link rel="shortcut icon" href="../../assets/media/logos/favicon.ico" />
+    <!--begin::Fonts(mandatory for all pages)-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" /> <!--end::Fonts-->
+    <!--begin::Vendor Stylesheets(used for this page only)-->
+    <link href="../../assets/plugins/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet" type="text/css" />
+    <!--end::Vendor Stylesheets-->
+
+
+    <!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
+    <link href="../../assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="../../assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+    <!--end::Global Stylesheets Bundle-->
+
+
+    <?php include '../../partials/trackers/_ga-tag-manager-for-head.php' ?>
+
+    <script>
+        // Frame-busting to prevent site from being loaded within a frame without permission (click-jacking)
+        if (window.top != window.self) {
+            window.top.location.replace(window.self.location.href);
+        }
+    </script>
+</head>
+<!--end::Head-->
+<!--begin::Body-->
+
+<body id="kt_app_body" data-kt-app-page-loading-enabled="true" data-kt-app-page-loading="on"
+    data-kt-app-header-fixed="true" data-kt-app-header-fixed-mobile="true" data-kt-app-toolbar-enabled="true"
+    class="app-default">
+
+    <?php include '../../partials/theme-mode/_init.php' ?>
+
+    <?php include '../../partials/trackers/_ga-tag-manager-for-body.php' ?>
+
+    <?php include '../../layout/partials/_page-loader.php' ?>
+
+    <?php include '../../layout/_default.php' ?>
+
+    <?php include '../../partials/_scrolltop.php' ?>
+
+    <!--begin::Javascript-->
+    <script>
+        var hostUrl = "../../assets/";        </script>
+    <!--begin::Global Javascript Bundle(mandatory for all pages)-->
+    <script src="../../assets/plugins/global/plugins.bundle.js"></script>
+    <script src="../../assets/js/scripts.bundle.js"></script>
+    <!--end::Global Javascript Bundle-->
+
+    <!--begin::Vendors Javascript(used for this page only)-->
+    <script src="../../assets/plugins/custom/fullcalendar/fullcalendar.bundle.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/percent.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/radar.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/map.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/geodata/worldLow.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/geodata/continentsLow.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/geodata/usaLow.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/geodata/worldTimeZonesLow.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/geodata/worldTimeZonesLow.js"></script>
+    <!--end::Vendors Javascript-->
+
+    <!--begin::Custom Javascript(used for this page only)-->
+    <script src="../../assets/js/widgets.bundle.js"></script>
+    <script src="../../assets/js/custom/widgets.js"></script>
+    <script src="../../assets/js/custom/apps/chat/chat.js"></script>
+    <script src="../../assets/js/custom/utilities/modals/upgrade-plan.js"></script>
+    <!--end::Custom Javascript-->
+    <!--end::Javascript-->
+</body>
+<!--end::Body-->
+
+</html>
